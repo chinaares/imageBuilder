da8e15ed8466ccaf64a0cb6b58e43e8ddbaa3303
Ares <yangzibin.cn@hotmail.com>
2016-09-22 11:18:50 +0800

    modify to fit local configure envirment

--- windows_2008_r2_cn_openstack/scripts/livecloud.ps1
+++ windows_2008_r2_cn_openstack/scripts/livecloud.ps1
@@ -15,15 +15,15 @@ function download($url, $dest) {
     ( New-Object System.Net.WebClient).DownloadFile( $url, $dest)
 }
 
-$url = "http://172.16.2.254/Packer/"
+$url = "http://192.166.69.12/Softwares/OS/"
 $bin_dir = "C:\Windows\System32\"
 $tmp_dir = "C:\Windows\Temp\"
 
 download $url"tools/7za.exe" $bin_dir"7za.exe"
 download $url"tools/curl.exe" $bin_dir"curl.exe"
 download $url"tools/virtiodriver2k8_openstack.tar.gz" $tmp_dir"virtiodriver.tar.gz"
-
-xzFile "C:\Windows\Temp\python.tar.gz" "C:\Windows\Temp"
+download $url"tools/virtio/guest-agent/qemu-ga-x64.msi" $tmp_dir"qemu-ga-x86_64.msi"
+download $url"tools/CloudbaseInitSetup_x64.msi" $tmp_dir"cloudbase.msi"
 
 if (!(Test-Path "C:\Windows\virtiodriver" )) {
     Write-Host "Install VirtIO Driver...."
@@ -44,3 +44,55 @@ if (!(Test-Path "C:\Windows\virtiodriver" )) {
         Write-Host "InstallVirtIO failed"
     }
 }
+
+if (!(Test-Path "C:\Program Files\qemu-ga" )) {
+    $msiFile = "C:\Windows\Temp\qemu-ga-x86_64.msi"
+    $targetdit = "C:\Program Files\qemu-ga"
+    $arguments = @(
+        "/i"
+        "`"$msiFile`""
+        "/qn"
+		"/l*v C:\Windows\Temp\qemu-ga-setup.log"
+        "/norestart"
+        "ALLUSERS=1"
+        "TARGETDIR=`"$targetdit`""
+    )
+    Write-Host "Installing $msiFile....."
+    $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+    if ($process.ExitCode -eq 0){
+        Write-Host "$msiFile has been successfully installed"
+        Start-Service "QEMU Guest Agent VSS Provider"
+        Start-Service "QEMU-GA"
+    } else {
+        Write-Host "installer exit code $($process.ExitCode) for file $($msifile)"
+    }   
+}
+
+if (!(Test-Path "C:\Program Files\Cloudbase Solutions\Cloudbase-Init" )) {
+    $msiFile = "C:\Windows\Temp\cloudbase.msi"
+    $targetdit = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init"
+    $arguments = @(
+        "/i"
+        "`"$msiFile`""
+        "/qn"
+        "/norestart"
+		"/l*v C:\Windows\Temp\CloudbaseInitSetup.log"
+        "ALLUSERS=1"
+		"LOGGINGSERIALPORTNAME=COM1"
+        "TARGETDIR=`"$targetdit`""
+    )
+    Write-Host "Installing $msiFile....."
+    $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+    if ($process.ExitCode -eq 0){
+        Write-Host "$msiFile has been successfully installed"
+        Start-Service "cloudbase-init"
+    } else {
+        Write-Host "installer exit code $($process.ExitCode) for file $($msifile)"
+    }   
+}
+
+if (Test-Path "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml") {
+    Write-Host "run sysprep to generalize the system (and shutdown)"
+    Start-Process "C:\Windows\System32\Sysprep\Sysprep.exe" "/quiet /generalize /oobe /quit /unattend:'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml'" -NoNewWindow -Wait
+}
+
