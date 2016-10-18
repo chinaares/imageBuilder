8748ecd7c73e69552929211d88eb0aceb570fa4f
Ares <yangzibin.cn@hotmail.com>
2016-10-18 09:29:28 +0800

    no desc

--- windows_10/scripts/livecloud.ps1
+++ windows_10/scripts/livecloud.ps1
@@ -15,46 +15,15 @@ function download($url, $dest) {
     ( New-Object System.Net.WebClient).DownloadFile( $url, $dest)
 }
 
-$url = "http://172.16.2.254/Packer/"
+$url = "http://192.166.69.12/Softwares/OS/"
 $bin_dir = "C:\Windows\System32\"
 $tmp_dir = "C:\Windows\Temp\"
 
 download $url"tools/7za.exe" $bin_dir"7za.exe"
 download $url"tools/curl.exe" $bin_dir"curl.exe"
-download $url"qga/vm_init.bat" $bin_dir"vm_init.bat"
-download $url"tools/python.tar.gz" $tmp_dir"python.tar.gz"
-download $url"tools/virtiodriver2012R2.tar.gz" $tmp_dir"virtiodriver.tar.gz"
-download $url"tools/vagent.tar.gz" $tmp_dir"vagent.tar.gz"
-download $url"qga/qemu-ga-x86_64.msi" $tmp_dir"qemu-ga-x86_64.msi"
-
-xzFile "C:\Windows\Temp\python.tar.gz" "C:\Windows\Temp"
-
-if (!(Test-Path "C:\Program Files\python" )) {
-    $msiFile = "C:\Windows\Temp\python-2.7.8.amd64.msi"
-    $python_home = "C:\Program Files\python"
-    $arguments = @(
-        "/i"
-        "`"$msiFile`""
-        "/qn"
-        "/norestart"
-        "ALLUSERS=1"
-        "TARGETDIR=`"$python_home`""
-    )
-    Write-Host "Installing $msiFile....."
-    $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
-    if ($process.ExitCode -eq 0){
-        Write-Host "$msiFile has been successfully installed"
-    } else {
-        Write-Host "installer exit code  $($process.ExitCode) for file  $($msifile)"
-    }
-    $pip = "C:\Windows\Temp\pip-7.1.2-py2.py3-none-any.whl"
-    $setuptools = "C:\Windows\Temp\setuptools-18.5-py2.py3-none-any.whl"
-    $pywin32 = "C:\Windows\Temp\pywin32-219.win-amd64-py2.7.exe"
-    & $python_home\python.exe $pip/pip install $pip
-    & $python_home\Scripts\pip.exe install $setuptools
-    & $python_home\Scripts\easy_install.exe $pywin32
-}
-[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\Program Files\python\;C:\Program Files\python\Scripts\", "User")
+download $url"tools/virtiodriver2012R2_openstack.tar.gz" $tmp_dir"virtiodriver.tar.gz"
+download $url"tools/virtio/guest-agent/qemu-ga-x64.msi" $tmp_dir"qemu-ga-x86_64.msi"
+download $url"tools/CloudbaseInitSetup_x64.msi" $tmp_dir"cloudbase.msi"
 
 if (!(Test-Path "C:\Windows\virtiodriver" )) {
     Write-Host "Install VirtIO Driver...."
@@ -74,21 +43,6 @@ if (!(Test-Path "C:\Windows\virtiodriver" )) {
     } else {
         Write-Host "InstallVirtIO failed"
     }
-}
-
-if (!(Test-Path "C:\Windows\vagent" )) {
-    Write-Host "Config Vagent...."
-    xzFile "C:\Windows\Temp\vagent.tar.gz" "C:\Windows"
-    $python = "C:\Program Files\python\python.exe"
-    & $python C:\Windows\vagent\vagent_service.py install
-    & $python C:\Windows\vagent\vagent_service.py start
-    Set-Service -Name "vagent" -StartupType Automatic -description "LiveCloud Agent for VM"
-    
-    # configure firewall
-    Write-Host "Configuring firewall"
-    netsh advfirewall firewall add rule name="vagent" dir=in action=allow service=vagent enable=yes
-    netsh advfirewall firewall add rule name="vagent" dir=in action=allow program="C:\Windows\vagent\vagent.py" enable=yes
-    netsh advfirewall firewall add rule name="vagent" dir=in action=allow protocol=TCP localport=12345
 }
 
 if (!(Test-Path "C:\Program Files\qemu-ga" )) {
@@ -111,4 +65,32 @@ if (!(Test-Path "C:\Program Files\qemu-ga" )) {
     } else {
         Write-Host "installer exit code $($process.ExitCode) for file $($msifile)"
     }   
+}
+
+if (!(Test-Path "C:\Program Files\Cloudbase Solutions\Cloudbase-Init" )) {
+    $msiFile = "C:\Windows\Temp\cloudbase.msi"
+    $targetdit = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init"
+    $arguments = @(
+        "/i"
+        "`"$msiFile`""
+        "/qn"
+        "/norestart"
+		"/l*v C:\Windows\Temp\CloudbaseInitSetup.log"
+        "ALLUSERS=1"
+		"LOGGINGSERIALPORTNAME=COM1"
+        "TARGETDIR=`"$targetdit`""
+    )
+    Write-Host "Installing $msiFile....."
+    $process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+    if ($process.ExitCode -eq 0){
+        Write-Host "$msiFile has been successfully installed"
+        Start-Service "cloudbase-init"
+    } else {
+        Write-Host "installer exit code $($process.ExitCode) for file $($msifile)"
+    }   
+}
+
+if (Test-Path "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml") {
+    Write-Host "run sysprep to generalize the system (and shutdown)"
+    Start-Process "C:\Windows\System32\Sysprep\Sysprep.exe" "/quiet /generalize /oobe /quit /unattend:'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\Unattend.xml'" -NoNewWindow -Wait
 }
