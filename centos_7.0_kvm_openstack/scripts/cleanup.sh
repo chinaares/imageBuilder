7193a461c6f9448235614e952fa8d97a0430ea7e
Ares <yangzibin.cn@hotmail.com>
2016-10-19 15:34:59 +0800

    no desc

--- centos_7.0_kvm_openstack/scripts/cleanup.sh
+++ centos_7.0_kvm_openstack/scripts/cleanup.sh
@@ -1,17 +1,33 @@
+#!/bin/bash
+
+set -x
+
 rm -fr /etc/yum.repos.d/custom.repo
+
+# Make sure Udev doesn't block our network
+# http://6.ptmc.org/?p=164
+echo "cleaning up udev rules"
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
 # Remove log files from the VM
 find /var/log -type f -exec rm -f {} \;
+
+# Remove locale seed
+rm -f /root/locale-preseed.cfg
 
 # rm  hosts keys
 rm -f /etc/ssh/*key*
 
 # Remove hardware specific settings from eth0
 sed -i -e 's/^\(HWADDR\|UUID\|IPV6INIT\|NM_CONTROLLED\|MTU\).*//;/^$/d' \
-        /etc/sysconfig/network-scripts/ifcfg-eth0
+    /etc/sysconfig/network-scripts/ifcfg-eth0
 # Remove all kernels except the current version
 rpm -qa | grep ^kernel-[0-9].* | sort | grep -v $(uname -r) | \
-        xargs -r yum -y remove
+    xargs -r yum -y remove
 yum -y clean all
+
 # Have a sane vimrc
 echo "set background=dark" >> /etc/vimrc
