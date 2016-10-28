892db811ad8dcdd500458b53d6789bad2000b47e
Ares <yangzibin.cn@hotmail.com>
2016-10-28 10:59:51 +0800

    fixed a build version,include docker installed. try to building ....
    Signed-off-by: Ares <yangzibin.cn@hotmail.com>

--- debian_8.x_kvm_openstack/build_kvm.sh
+++ debian_8.x_kvm_openstack/build_kvm.sh
@@ -3,8 +3,19 @@
 set -x
 
 : ${BUILD_VERSION:="v$(date +'%Y%m%d%H%M%S')"}
-: ${BUILD_NAME:="Debian_8.3.0-x86_64_openstack"}
-: ${VM_NAME:="debian83"}
+: ${BUILD_NAME:="Debian_8-x86_64_openstack"}
+: ${VM_NAME:="debian8_openstack"}
+: ${TEMPLATE_FILE:="template_kvm.json"}
+: ${DISK_DIR:="disk"}
+
+if  [ "$1" = "docker" ]
+then
+   echo "**** Build image with docker engine installed. *****"
+   BUILD_NAME="${BUILD_NAME}_d"
+   VM_NAME="${VM_NAME}_d"
+   TEMPLATE_FILE="template_kvm_docker.json"
+   DISK_DIR="disk_d"
+fi
 
 export BUILD_NAME
 export VM_NAME
@@ -14,9 +25,9 @@ PWD=`pwd`
 FILENAME=${VM_NAME}
 PACKER=/usr/bin/packer
 
-if [ -e "${PWD}/disk" ];
+if [ -e "${PWD}/${DISK_DIR}" ];
 then
-    rm -rf ${PWD}/disk
+    rm -rf ${PWD}/${DISK_DIR}
 fi
 
 if [ ! -e "${PWD}/final_images" ];
@@ -24,14 +35,14 @@ then
     mkdir -pv ${PWD}/final_images
 fi
 
-$PACKER build template_kvm.json
+$PACKER build ${TEMPLATE_FILE}
 
-cd disk
+cd ${DISK_DIR}
 qemu-img convert -c -O qcow2 $FILENAME ${BUILD_NAME}-${BUILD_VERSION}.qcow2
 cd -
 
-mv ${PWD}/disk/${BUILD_NAME}-${BUILD_VERSION}.qcow2 ${PWD}/final_images
-rm -rf ${PWD}/disk
+mv ${PWD}/${DISK_DIR}/${BUILD_NAME}-${BUILD_VERSION}.qcow2 ${PWD}/final_images
+rm -rf ${PWD}/${DISK_DIR}
 echo "==> Generate files:"
 find ${PWD}/final_images -type f -printf "==> %f\n"
 
--- debian_8.x_kvm_openstack/scripts/base.sh
+++ debian_8.x_kvm_openstack/scripts/base.sh
@@ -1,11 +1,62 @@
 cat <<'EOF' > /etc/apt/sources.list
-deb http://ftp.cn.debian.org/debian/ jessie main contrib non-free
-deb-src http://ftp.cn.debian.org/debian/ jessie main contrib non-free
-deb http://ftp.cn.debian.org/debian-security/ jessie/updates main
-deb-src http://ftp.cn.debian.org/debian-security/ jessie/updates main
+deb http://mirrors.ustc.edu.cn/debian jessie main contrib non-free
+deb-src http://mirrors.ustc.edu.cn/debian jessie main contrib non-free
+deb http://mirrors.ustc.edu.cn/debian jessie-proposed-updates main contrib non-free
+deb-src http://mirrors.ustc.edu.cn/debian jessie-proposed-updates main contrib non-free
+deb http://mirrors.ustc.edu.cn/debian jessie-updates main contrib non-free
+deb-src http://mirrors.ustc.edu.cn/debian jessie-updates main contrib non-free
+deb http://mirrors.ustc.edu.cn/debian-security/ jessie/updates main contrib non-free
+deb-src http://mirrors.ustc.edu.cn/debian-security/ jessie/updates main contrib non-free
 EOF
+
+cat <<'EOF' > /etc/apt/sources.list
+deb http://mirrors.163.com/debian/ jessie main contrib non-free
+deb-src http://mirrors.163.com/debian jessie main contrib non-free
+deb http://mirrors.163.com/debian/ jessie-proposed-updates main contrib non-free
+deb-src http://mirrors.163.com/debian/ jessie-proposed-updates main contrib non-free
+deb http://mirrors.163.com/debian/ jessie-updates main contrib non-free
+deb-src http://mirrors.163.com/debian/ jessie-updates main contrib non-free
+deb http://mirrors.163.com/debian-security/ jessie/updates main contrib non-free
+deb-src http://mirrors.163.com/debian-security/ jessie/updates main contrib non-free
+EOF
+
+echo "
+Acquire::http::Proxy \"http://192.161.14.179:3142\";
+Acquire::https::Proxy \"http://192.161.14.179:3142\";
+Acquire::ftp::Proxy \"http://192.161.14.179:3142\";
+"| sudo tee /etc/apt/apt.conf.d/90-apt-proxy.conf
+
 apt-get update
-apt-get install -y --force-yes chkconfig libglib2.0-0 curl 
+apt-get -y --force-yes dist-upgrade
+
+#Unable to correct missing packages.
+#E: Failed to fetch http://mirrors.ustc.edu.cn/debian/pool/main/liby/libyaml/libyaml-0-2_0.1.6-3_amd64.deb  Size mismatch
+wget http://mirrors.ustc.edu.cn/debian/pool/main/liby/libyaml/libyaml-0-2_0.1.6-3_amd64.deb \
+-O /tmp/libyaml-0-2_0.1.6-3_amd64.deb  && \
+dpkg -i /tmp/libyaml-0-2_0.1.6-3_amd64.deb 
+#E: Failed to fetch http://mirrors.ustc.edu.cn/debian/pool/main/e/emacsen-common/emacsen-common_2.0.8_all.deb  Size mismatch
+wget http://mirrors.ustc.edu.cn/debian/pool/main/e/emacsen-common/emacsen-common_2.0.8_all.deb \
+-O /tmp/emacsen-common_2.0.8_all.deb  && \
+dpkg -i /tmp/emacsen-common_2.0.8_all.deb
+#E: Failed to fetch http://mirrors.ustc.edu.cn/debian/pool/main/n/nmon/nmon_14g+debian-1_amd64.deb  Size mismatch
+wget http://mirrors.ustc.edu.cn/debian/pool/main/n/nmon/nmon_14g+debian-1_amd64.deb \
+-O /tmp/nmon_14g+debian-1_amd64.deb  && \
+dpkg -i /tmp/nmon_14g+debian-1_amd64.deb
+
+PACKAGES="
+chkconfig
+libglib2.0-0
+curl
+emacs24-nox
+htop
+nmon
+slurm
+tcpdump
+unzip
+vim-nox
+sudo
+"
+apt-get install -y --force-yes $PACKAGES
 
 cat <<'EOF' > /root/.bashrc
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
